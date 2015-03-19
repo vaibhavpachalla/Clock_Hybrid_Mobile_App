4bbfa7ae802eb33692ee7230e6a3a9873f4d7d8c
vaibhavpachalla <vaibhav.pachalla@gmail.com>
2015-03-19 13:50:26 -0400

    MultipleCities

--- js/silly.js
+++ js/silly.js
@@ -1,7 +1,28 @@
 var SILLY = (function(module) {
 
 	module.DoIt = function(resultObject) {
-		resultObject.prepend(Date() + '<br/>');
+		function calcTime(city, offset) {
+    // create Date object for current location
+    var d = new Date();
+
+    // convert to msec
+    // subtract local time zone offset
+    // get UTC time in msec
+    var utc = d.getTime() - (d.getTimezoneOffset() * 60000);
+
+    // create new Date object for different city
+    // using supplied offset
+    var nd = new Date(utc + (3600000*offset));
+
+    // return time as a string
+    return "The time for "+ city +" is "+ nd.toUTCString();
+	}
+	var hyderabadTime = calcTime('Hyderabad', '+5');
+	var charlotteTime = calcTime('Charlotte', '-4');
+	var houstonTime = calcTime('Houston', '-5');
+	var denverTime = calcTime('Denver' , '-6');
+	var losAngelesTime = calcTime('Los Angeles', '-7')
+	resultObject.prepend(hyderabadTime + '<br/>' + charlotteTime + '<br/>' + houstonTime + '<br/>' + denverTime + '<br/>' + losAngelesTime + '<br/>');
 	};
 	
 	return module;
